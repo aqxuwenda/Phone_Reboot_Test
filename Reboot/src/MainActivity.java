cd2f4ada848d02b8ac027b4b7c761dd958654d6f
aqxuwenda <aqxuwenda@163.com>
2016-05-10 11:56:44 +0800

    Add Auto Reboot Test Counts File
    添加FileWrite()&FileRead()

--- Reboot/src/MainActivity.java
+++ Reboot/src/MainActivity.java
@@ -1,5 +1,12 @@
 package com.longcheer.reboot;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PowerManager;
@@ -9,9 +16,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+@SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
 	static final String REBOOT_TEST_COUNT_FILE = "/data/tmp/StressRebootCount";
+	private FileInputStream fis;
+	private FileOutputStream fos;
 	boolean flag = true;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +36,36 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				PowerManager pms = (PowerManager) getSystemService(POWER_SERVICE);
-				EditText seconds = (EditText) findViewById(R.id.seconds);
-				int waittime = Integer.parseInt(seconds.getText().toString());
+//				EditText seconds = (EditText) findViewById(R.id.seconds);
+//				int waittime = Integer.parseInt(seconds.getText().toString());
 				EditText counts = (EditText) findViewById(R.id.counts);
 				int count = Integer.parseInt(counts.getText().toString());
-				int i = 0;
-				while (count > 0 || i < count || !flag) {
-					try {
-						Thread.sleep(waittime);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					i++;
-					Toast.makeText(MainActivity.this, "第"+i+"次", Toast.LENGTH_LONG).show();
+//				int i = 0;
+//				while (count > 0 || i < count || !flag) {
+//					try {
+//						Thread.sleep(waittime);
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					i++;
+//
+//					Toast.makeText(MainActivity.this, "---"+FileRead()+"---", Toast.LENGTH_LONG).show();
+//
+//	
+//					pms.reboot(null);
+//					
+//				}
+				FileWrite(count);
+				if(FileRead()>0){
+					int c = FileRead()-1;
+					FileWrite(c);
+					Toast.makeText(MainActivity.this, "---"+FileRead()+"---", Toast.LENGTH_LONG).show();
 					pms.reboot(null);
-					
 				}
-
 			}
-
 		});
+		
 		stop.setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -57,20 +76,47 @@ public class MainActivity extends Activity {
 		});
 	}
 	
-	protected void FileWrite(int count) throw Exception{
-		File f = new File(REBOOT_TEST_COUNT_FILE);
-		if(!f.exists()){
-			f.createNewFile();
+	protected void FileWrite(int count) {
+		try {
+			File f = new File(REBOOT_TEST_COUNT_FILE);
+			if(!f.exists()){
+				f.createNewFile();
+			}
+			fos = new FileOutputStream(REBOOT_TEST_COUNT_FILE);
+			fos.write(count);
+			fos.flush();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		f.write(count);
-		f.flush();
-		f.close();
 	}
 	
-	protected int FileRead() throw Exception{
-		File f = new File(REBOOT_TEST_COUNT_FILE);
-		f.read();
-		f.close();
-		return 0;
+	protected int FileRead() {
+		int count = 0;
+		try {
+			File f = new File(REBOOT_TEST_COUNT_FILE);
+			if(!f.exists()){
+				return 0;
+			}
+			fis = new FileInputStream(REBOOT_TEST_COUNT_FILE);
+			count = fis.read();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
 	}
 }
